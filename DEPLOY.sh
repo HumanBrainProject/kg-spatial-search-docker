63e01b31eb3fa2d133ab83450b2c56ead3508172
Lionel Sambuc <lionel.sambuc@epfl.ch>
2018-07-05 16:50:24 +0200

    Allow option definition from the command line

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -6,28 +6,28 @@
 # -----------------------------------------------------------------------------
 
 # Options for fetching HBP-Lucene-Solr source files
-SRC="https://github.com/HumanBrainProject/kg-spatial-search.git" ;
+: ${SRC:="https://github.com/HumanBrainProject/kg-spatial-search.git"};
 
 # Options for image building
-TAG="hbp-lucene-solr" ;
-NOW=`date -u +"%Y-%m-%dT%H:%M:%SZ"` ;
-REF=`git -C ./src/ rev-parse --short HEAD` ;
-NOCACHE="--no-cache=true" ; # eps., if sources were updated
-build_opts="-t ${TAG} ${NOCACHE} --build-arg BUILD_DATE=${NOW} --build-arg VCS_REF=${REF}" ;
+: ${TAG:="hbp-lucene-solr"};
+: ${NOW:=`date -u +"%Y-%m-%dT%H:%M:%SZ"`};
+: ${REF:=`git -C ./src/ rev-parse --short HEAD`};
+: ${NOCACHE:="--no-cache=true"}; # eps., if sources were updated
+build_opts="-t ${TAG} ${NOCACHE} --build-arg BUILD_DATE=${NOW} --build-arg VCS_REF=${REF}";
 
 # Options for clean-run
-MEM="7g" ; # memory for JVM
-NAM="my-hbp-solr" ;
-INIT="INIT_SOLR_HOME=yes" ;
-ENV="-e SOLR_HOME=/opt/custom-solr-home-1 -e SOLR_HEAP=${MEM}" ;
-VOL="$PWD/docker-volumes/custom-solr-home-1:/opt/custom-solr-home-1" ;
-POR="8983:8983" ; # port
+: ${MEM:="7g"}; # memory for JVM
+: ${NAM:="my-hbp-solr"};
+: ${INIT:="INIT_SOLR_HOME=yes"};
+: ${ENV:="-e SOLR_HOME=/opt/custom-solr-home-1 -e SOLR_HEAP=${MEM}"};
+: ${VOL:="$PWD/docker-volumes/custom-solr-home-1:/opt/custom-solr-home-1"};
+: ${POR:="8983:8983"}; # port
 # --name ${NAM}
-run_opts="-d -p ${POR} -v ${VOL} ${ENV} -e ${INIT} ${TAG}" ; # '-it'
+run_opts="-d -p ${POR} -v ${VOL} ${ENV} -e ${INIT} ${TAG}"; # '-it'
 
 # Options for (re)-run using previous SOLR_HOME
-NOINIT="INIT_SOLR_HOME=no" ;
-rerun_opts="-d -p ${POR} -v ${VOL} ${ENV} -e ${NOINIT} ${TAG}" ; # '-it'
+: ${NOINIT:="INIT_SOLR_HOME=no"};
+rerun_opts="-d -p ${POR} -v ${VOL} ${ENV} -e ${NOINIT} ${TAG}"; # '-it'
 
 echo "--> HBP Lucene-Solr Docker image: what do you want to do? <--"
 select cmd in "Clone" "Build" "Run" "Resume" "Stop" "Cancel"; do
